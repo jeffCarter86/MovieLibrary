5b82c34d58ea5a14bd794cba3a0616039ad3626a
Jeff <jeffrey.r.carter86@gmail.com>
2020-06-02 13:26:59 -0400

    started javascript setup add movie function

--- WebAPISample/app.js
+++ WebAPISample/app.js
@@ -1,1 +1,16 @@
-﻿
+﻿"use strict";
+
+var movieIds = new Object;
+
+$('#my-form').submit(addMovie);
+
+function addMovie(e) {
+    $.ajax({
+        type: "POST",
+        async: false,
+        url: "https://localhost:44325/api/movie",
+        contentType: 'application/json',
+        data: JSON.stringify({ Title: this["title"].value, Director: this["director"].value, Genre: this["genre"].value })
+    });
+    // e.preventDefault();
+}
