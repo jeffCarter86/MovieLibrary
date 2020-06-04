c2616f760965fe8735fb3058605f37d5a198f414
Jeff <jeffrey.r.carter86@gmail.com>
2020-06-04 08:35:40 -0400

    added genre input to index

--- Client/app.js
+++ Client/app.js
@@ -1,29 +1,86 @@
-function retrieveMovie(){
-    var 
+﻿"use strict";
+
+var movieIds = new Object;
+
+$('#my-form').submit(addMovie );
+
+function addMovie(e){
+    $.ajax({
+        type: "POST",
+        async: false,
+        url: "https://localhost:44325/api/movie",
+        contentType: 'application/json',
+        data: JSON.stringify({Title: this["title"].value, Director: this["director"].value, Genre: this["genre"].value})
+    });
+    // e.preventDefault();
 }
-(function($){
-    function processForm( e ){
-        var dict = {
-        	Title : this["title"].value,
-        	Director: this["director"].value
-        };
 
-        $.ajax({
-            url: 'https://localhost:44325/api/movie',
-            dataType: 'json',
-            type: 'post',
-            contentType: 'application/json',
-            data: JSON.stringify(dict),
-            success: function( data, textStatus, jQxhr ){
-                $('#response pre').html( data );
-            },
-            error: function( jqXhr, textStatus, errorThrown ){
-                console.log( errorThrown );
-            }
-        });
+function updateMovie(id, title, director, genre){
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        type: "PUT",
+        async: false,
+        dataType: "json",
+        url: "https://localhost:44325/api/movie",
+        success: location.reload(),
+        contentType: 'application/json',
+        data: JSON.stringify({MovieId: id, Title: title, Director: director, Genre: genre})
+    });
+};
 
-        e.preventDefault();
-    }
+function editForm(rowId){
+    var rowEdit = 
+    // "<div id=\"edit\">" +
+    "<td>" + "<input type='text' id='title" + rowId + "' value='" + $("#" + rowId + " td:nth-child(1)").html().toString() + "'></td>" +
+    "<td>" + "<input type='text' id='director" + rowId + "' value='" + $("#" + rowId + " td:nth-child(2)").html().toString() + "'></td>" +
+    "<td>" + "<input type='text' id='genre" + rowId + "' value='" + $("#" + rowId + " td:nth-child(3)").html().toString() + "'></td>" +
+    "<td>" + "<button onclick='updateMovie(" + movieIds[rowId] + ", " + "$(\"input#title" + rowId + "\").val()" + ", " + "$(\"input#director" + rowId + "\").val()" + ", " + "$(\"input#genre" + rowId + "\").val()" + ")'>Submit</button></td>";
+    // + "</div>";
+    $("#" + rowId).html(rowEdit);
+}
 
-    $('#my-form').submit( processForm );
-})(jQuery);
+function deleteMovie(movieId){
+    $.ajax({
+        type: "DELETE",
+        async: false,
+        dataType: "json",
+        url: "https://localhost:44325/api/movie/" + movieId,
+        success: location.reload(),
+        contentType: 'application/json'
+    });
+}
+
+$(document).ready(function(){
+    // queried database for list of movie object
+    $.ajax({
+        type: "GET", 
+        url: "https://localhost:44325/api/movie",
+        contentType: "application/json; charset = utf-8",    
+        dataType: "json",
+
+        // manipulated DOM to append rows to movie table
+        success: function(data){
+            // $("#movie-list").html('');
+            movieIds = new Object;
+           $.each(data, function (i, item){
+               movieIds["movie" + i] = item.movieId;
+               var movie = "<tr id='movie"+ i + "'>" +
+               "<td>" + item.title + "</td>" +
+               "<td>" + item.director + "</td>" +
+               "<td>" + item.genre + "</td>" +
+               "<td><button onclick='editForm(\"movie" + i + "\")'>Edit</button></td>" + 
+               "<td><button onclick='deleteMovie(movieIds[\"movie" + i + "\"])'>Delete</button>" +
+               "</tr>";
+
+               $('#movie-list').append(movie);
+           })
+        }
+    });
+});
+
+function showAddMovie(){
+    document.getElementById("my-form").style.display = "inline";
+}
